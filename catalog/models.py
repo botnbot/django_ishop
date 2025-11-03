3393d7f0cfff6cf9ac8c5330571f5193cc361ab4
Sergey Korobeynikov <skoro@smn35.com>
2025-11-03 22:00:54 +0300

    Product & Category models created in catalog app Made migration Made superuser

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
+    category = models.ForeignKey(to='Category', on_delete=models.CASCADE, verbose_name='Категория',
+                                 related_name='products')
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена за покупку')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='дата изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.drscription}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name', 'purchase_price']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name']
