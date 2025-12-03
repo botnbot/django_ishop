99436b8f48fb167efc734b0067811de7b5de646d
Sergey Korobeynikov <skoro@smn35.com>
2025-12-03 17:40:59 +0300

    stylized & validate forms

--- catalog/models.py
+++ catalog/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 
 
+from django.db import models
+
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name="Наименование")
     description = models.TextField(verbose_name="Описание")
@@ -20,7 +22,8 @@ class Product(models.Model):
         related_name="products",
     )
     price = models.DecimalField(
-        max_digits=10, decimal_places=2, verbose_name="цена за покупку"
+        max_digits=10, decimal_places=2,
+        verbose_name="цена за покупку"
     )
     created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateField(auto_now=True, verbose_name="Дата изменения")
@@ -28,10 +31,6 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.name} {self.description}"
 
-    class Meta:
-        verbose_name = "продукт"
-        verbose_name_plural = "продукты"
-        ordering = ["name", "price"]
 
 
 class Category(models.Model):
---
+++ catalog/widgets.py
@@ -0,0 +1,5 @@
+from django.forms.widgets import ClearableFileInput
+
+class CustomClearableFileInput(ClearableFileInput):
+    template_name = 'catalog/templates/catalog/custom_clearable_file_input.html'
+    clear_checkbox_label = 'Удалить текущее изображение'
