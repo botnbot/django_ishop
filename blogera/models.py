a7bccc875e47a226c9223061a15dbcf842a84cb2
Sergey Korobeynikov <skoro@smn35.com>
2025-12-10 16:41:51 +0300

    success registration welcome email sending realized

--- blogera/models.py
+++ blogera/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from django.conf import settings
 
 
 class Post(models.Model):
@@ -8,6 +11,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False, verbose_name="Опубликовать")
     views_count = models.PositiveIntegerField(default=0, verbose_name="Счетчик просмотров")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
 
     def __str__(self):
         return f"{self.title} ({self.created_at.strftime('%d.%m.%Y')})"
--- users/views.py
+++ users/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
 
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+
+from  django.conf import settings
 from .forms import CustomUserCreationForm
 
 class RegisterView(CreateView):
     template_name = 'users/register.html'
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('catalog:product_list')
+    success_url = reverse_lazy('catalog:products_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)  # <-- здесь создаётся пользователь
+        user_email = self.object.email  # <-- теперь self.object — это новый пользователь
+
+        send_mail(
+            subject='Добро пожаловать!',
+            message='Спасибо за регистрацию на сайте!',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user_email],
+        )
+
+        return response
