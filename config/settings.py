12eec787041d97ea594553001ebcfed08e172509
Sergey Korobeynikov <skoro@smn35.com>
2025-12-10 16:41:51 +0300

    formatted

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,16 @@ LOGIN_URL = reverse_lazy('users:login')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
