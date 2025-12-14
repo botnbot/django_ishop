1210bb04383bd587a063d6aec450beaf67564b23
Sergey Korobeynikov <skoro@smn35.com>
2025-12-14 12:54:58 +0300

    is_published field added to Product model migrate some navbar changes in menu.html small changes in base.html README.md changed

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-
 # Create your models here.
 
 
@@ -24,6 +21,7 @@ class Product(models.Model):
     )
     created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateField(auto_now=True, verbose_name="Дата изменения")
+    is_published = models.BooleanField(default=False,verbose_name="Опубликован")
 
     def __str__(self):
         return f"{self.name} {self.description}"
