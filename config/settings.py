5e23871029ffad215e0a5fa0b0e16fc46b792916
Sergey Korobeynikov <skoro@smn35.com>
2026-01-29 12:29:27 +0300

    settings.py corrected

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 # Загружаем переменные окружения из файла .env
 load_dotenv(override=True)
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
