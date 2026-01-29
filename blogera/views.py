f2deaf15c725ec11379b4339e9c48b52f32e1053
Sergey Korobeynikov <skoro@smn35.com>
2026-01-29 12:49:18 +0300

    some views imports in blogera\views added views imports in catalog\views changed views imports in users\views changed

--- blogera/views.py
+++ blogera/views.py
@@ -1,19 +1,25 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import (
-    ListView,
-    DetailView,
-    DeleteView,
-    CreateView,
-    UpdateView,
-)
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
 from blogera.models import Post
 
 
+class AuthorOrStaffMixin(UserPassesTestMixin):
+    """Миксин для проверки прав: автор или staff"""
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.author or self.request.user.is_staff
+
+    def handle_no_permission(self):
+        messages.error(self.request, "У вас нет прав для этого действия")
+        return redirect("blogera:post_list")
+
+
 class PostListView(ListView):
     model = Post
-    fields = ["title", "created_at"]
     template_name = "blogera/post_list.html"
     context_object_name = "posts"
 
@@ -28,20 +34,16 @@ class PostDetailsView(DetailView):
 
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
-        self.object.views_count += 1
-        self.object.save()
+        # инкрементируем счетчик просмотров оптимально
+        Post.objects.filter(pk=self.object.pk).update(views_count=self.object.views_count + 1)
         return self.object
 
 
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class PostDeleteView(LoginRequiredMixin, AuthorOrStaffMixin, DeleteView):
     model = Post
     template_name = "blogera/confirm_post_delete.html"
     success_url = reverse_lazy("blogera:post_list")
     context_object_name = "post"
-
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author or self.request.user.is_staff
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -49,14 +51,13 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     fields = ["title", "content", "image", "is_published"]
     template_name = "blogera/post_create.html"
     success_url = reverse_lazy("blogera:post_list")
-    context_object_name = "post"
 
     def form_valid(self, form):
-        form.instance.author = self.request.user  # автоматически назначает автора посту
+        form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, AuthorOrStaffMixin, UpdateView):
     model = Post
     fields = ["title", "content", "image", "is_published"]
     template_name = "blogera/post_update.html"
@@ -64,8 +65,3 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
     def get_success_url(self):
         return reverse_lazy("blogera:post_details", kwargs={"pk": self.object.pk})
-
-    def test_func(self):
-        """Проверяет, что пользователь автор поста или админ."""
-        post = self.get_object()
-        return self.request.user == post.author or self.request.user.is_staff
--- catalog/views.py
+++ catalog/views.py
@@ -1,44 +1,22 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views import View
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView,
-)
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic.edit import FormView
 
-from catalog.forms import ProductForm
+from catalog.forms import ProductForm, ContactForm
 from catalog.models import Product
 
 
-class ProductsCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
-    model = Product
-    form_class = ProductForm
-    template_name = "catalog/product_form.html"
-    success_url = reverse_lazy("catalog:product_list")
-
+class StaffRequiredMixin(UserPassesTestMixin):
+    """Миксин для проверки, что пользователь является staff"""
     def test_func(self):
         return self.request.user.is_staff
 
-
-class ProductsUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Product
-    form_class = ProductForm
-    template_name = "catalog/product_form.html"
-    success_url = reverse_lazy("catalog:product_list")
-
-    def form_valid(self, form):
-        if form.cleaned_data.get("image-clear") and form.instance.image:
-            form.instance.image.delete(save=False)
-            form.instance.image = None
-        return super().form_valid(form)
-
-    def test_func(self):
-        return self.request.user.is_staff
+    def handle_no_permission(self):
+        messages.error(self.request, "У вас нет прав для этого действия")
+        return redirect("catalog:products_list")
 
 
 class ProductsListView(ListView):
@@ -50,29 +28,35 @@ class ProductsListView(ListView):
 class ProductDetailsView(LoginRequiredMixin, DetailView):
     model = Product
     template_name = "catalog/product_details.html"
-    form_class = ProductForm
     context_object_name = "product"
 
 
-class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class ProductsCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:products_list")
+
+
+class ProductsUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:products_list")
+
+
+class ProductDeleteView(LoginRequiredMixin, StaffRequiredMixin, DeleteView):
     model = Product
     template_name = "catalog/product_confirm_delete.html"
     context_object_name = "product"
-    success_url = reverse_lazy("catalog:product_list")
-
-    def test_func(self):
-        return self.request.user.is_staff
+    success_url = reverse_lazy("catalog:products_list")
 
 
-class ContactsView(LoginRequiredMixin, View):
+class ContactsView(LoginRequiredMixin, FormView):
     template_name = "catalog/contacts.html"
+    form_class = ContactForm
+    success_url = reverse_lazy("catalog:contacts")
 
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
-
-    def post(self, request, *args, **kwargs):
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+    def form_valid(self, form):
+        messages.success(self.request, "Сообщение успешно отправлено")
+        return super().form_valid(form)
