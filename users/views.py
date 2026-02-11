99eb6e38fa25c959fc3637d58218d89188f31265
Sergey Korobeynikov <skoro@smn35.com>
2026-02-11 20:18:13 +0300

    send email after registration corrected
    dispatch attribute replaced with post in CustomLogoutView

--- users/views.py
+++ users/views.py
@@ -1,43 +1,49 @@
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
-from django.contrib.auth.views import LoginView, LogoutView
 
-from django.conf import settings
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
 
-# ------------------ Регистрация ------------------ #
+from django.shortcuts import redirect
+
+
 class RegisterView(CreateView):
     template_name = "users/register.html"
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("catalog:product_list")
 
     def form_valid(self, form):
-        response = super().form_valid(form)
+        self.object = form.save()
         user_email = self.object.email
 
-        # Отправка приветственного письма
+        login(self.request, self.object)
+
         send_mail(
             subject="Добро пожаловать!",
             message="Спасибо за регистрацию на сайте!",
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[user_email],
+            fail_silently=False,
         )
 
-        # Автоматический вход пользователя после регистрации
-        login(self.request, self.object)
-
-        # Сообщение на сайте
         messages.success(
             self.request,
             "Регистрация прошла успешно! На вашу почту отправлено приветственное письмо."
         )
-        return response
+
+        return redirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        print("FORM INVALID:", form.errors)
+        return super().form_invalid(form)
+
 
 
 # ------------------ Вход ------------------ #
@@ -45,7 +51,7 @@ class CustomLoginView(LoginView):
     template_name = "users/login.html"
 
     def form_valid(self, form):
-        messages.success(self.request, f"Вы успешно вошли как {form.get_user().username}")
+        messages.success(self.request, f"Вы успешно вошли как {form.get_user().email}")
         return super().form_valid(form)
 
 
@@ -53,9 +59,9 @@ class CustomLoginView(LoginView):
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy("catalog:product_list")
 
-    def dispatch(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         messages.success(request, "Вы вышли из аккаунта")
-        return super().dispatch(request, *args, **kwargs)
+        return super().post(request, *args, **kwargs)
 
 
 # ------------------ Редактирование профиля ------------------ #
